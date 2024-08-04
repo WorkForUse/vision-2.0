51493614a6e3e1b01366d7aac08e8af50b9f018f
WorkForUse <forwork.hs2@gmail.com>
2024-08-04 05:25:26 -0700

    add more work

---
+++ topFourNiches/GraphicsDesigns.php
@@ -0,0 +1,396 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Flexerr | Freelance Service</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- -------- -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <!-- Navbar section -->
+
+  <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+
+  <div class="container-fluid bg-light text-black">
+
+     
+
+     <div class="input-group">
+
+      <div><a href="#" class="text-black text-decoration-none"><img style="filter: hue-rotate(90deg) !important;" src="./img/logo.svg" width="10%"><span class="fw-bold fs-3"> &nbsp; flexx</span> &nbsp; <cite> Hours </cite></a></div>
+
+      <div class="d-flex p-2">
+      <input type="text" class="form-control" placeholder="Enter service name" aria-label="Recipient's username" aria-describedby="button-addon2">
+      <button class="btn btn-dark " type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </div>
+
+    </div>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Explore
+          </a>
+
+          <ul class="dropdown-menu" style="width: 330px; height: 340px;">
+
+            <li>
+              <a class="dropdown-item fw-bold" href="#">Discover</a>
+              <span class="ms-3">Inspiring projects made on flexx</span> <br>
+            </li> <hr>
+
+            <li>
+              <a class="dropdown-item fw-bold" href="#">Community</a>
+              <span class="ms-3">Connect with flexx team & community</span>
+            </li> <hr>
+
+            <li>
+              <a class="dropdown-item fw-bold" href="#">Guides</a>
+              <span class="ms-3">In-depth guides covering business topics</span>
+            </li> <hr>
+
+            <li>
+              <a class="dropdown-item fw-bold" href="#">Learn</a>
+              <span class="ms-3">Professional online courses, led by experts</span>
+            </li> <hr>
+
+          </ul>
+
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">preferences</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Seller</a>
+        </li>
+        <li class="nav-item">
+          <button class="px-3 border-0 rounded"><a class="nav-link text-success fw-bold" href="#">Join</a></button>
+        </li>
+
+      </ul>
+
+    </div>
+
+  </div>
+
+</nav>
+
+<!-- ------ -->
+
+<!-- banner section -->
+
+<div class="container text-white my-4 p-5 rounded-4" style="background: linear-gradient(180deg, #003912 36.72%, #19aa64 141.43%);"
+>
+    <div class="row">
+
+      <div class="col-md-8">
+
+        <div class="d-flex p-2">
+          <input type="text" class="form-control py-2" placeholder="Here to Search your interest" aria-label="searchbar" aria-describedby="button-addon2">
+        <button class="btn btn-dark " type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </div> <br> 
+
+        <h1>Make Bright Ideas Happen</h1>
+        <p>Access global talent on the freelancer website trusted by over 1 million businesses worldwide.
+        </p>
+
+      </div>
+
+      <div class="col-md-4">
+
+        <div>
+          <img src="./img/bannerImg/bannerImage.png" style="filter: hue-rotate(320deg) !important;" class="float-end">
+        </div>
+
+      </div>
+    </div>
+</div>
+
+<!-- ------ -->
+
+<!-- category_selection -->
+
+  <div class="container text-center">
+
+    <div class="row" style="gap: 5rem;">
+
+      <div class="col border rounded-4 p-4 shadow-lg p-3 mb-5 bg-white rounded border-white">
+        <a href="#" class="text-decoration-none">
+          <img src="./img/svg/full-stack-developer-icon.svg" width="15%">
+          <h5 class="text-black fs-6">Programming & Tech</h5>
+        </a>
+      </div>
+
+      <div class="col border rounded-4 p-4 shadow-lg p-3 mb-5 bg-white rounded border-white">
+        <a href="#" class="text-decoration-none">
+          <img src="./img/svg/pen-tool-icon.svg" width="15%">
+          <h5 class="text-black fs-6">Graphic Designs</h5>
+        </a>
+      </div>
+
+      <div class="col border rounded-4 p-4 shadow-lg p-3 mb-5 bg-white rounded border-white">
+        <a href="#" class="text-decoration-none">
+        <img src="./img/svg/artificial-intelligence-icon.svg" width="15%">
+        <h5 class="text-black fs-6">Artificial Inteligence</h5>
+      </a>
+      </div>
+
+      <div class="col border rounded-4 p-4 shadow-lg p-3 mb-5 bg-white rounded border-white">
+        <a href="#" class="text-decoration-none">
+          <img src="./img/svg/internet-marketing-icon.svg" width="15%">
+          <h5 class="text-black fs-6">Digital Marketing</h5>
+        </a>
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Recently Viewed & More -->
+
+    <div class="container">
+      <h1 class="fw-light my-3 fs-2">Recently Viewed & More</h1>
+      <div class="row container">
+
+        <div class="col p-1">
+
+          <a href="#"><img class="rounded" src="./img/recent/mobile_app_development.jpg"></a>
+          <span><a href="#" class="text-black text-decoration-none">Waheed Ahmed <pre style="margin-right: 28px !important; margin-top: 2px;" class="badge text-bg-success d-inline float-end">Level 1</pre></a></span> <br>
+
+          <p><a href="#" class="text-black text-decoration-none fw-light">I will do ios and android mobile app development using flutter...</a></p>
+
+          <p><i class="fa-solid fa-star text-success"></i> 4.9 (17) <sub class="fs-5 float-end" style="margin-right: 42px !important;">From PKR 35,299</sub></p>
+
+        </div>
+
+        <div class="col p-1">
+
+          <a href="#"><img class="rounded" src="./img/recent/business_leads.PNG"></a>
+
+          <span><a href="#" class="text-black text-decoration-none">Waheed Ahmed <pre style="margin-right: 28px !important; margin-top: 2px;" class="badge text-bg-success d-inline float-end">Level 1</pre></a></span> <br>
+
+          <p><a href="#" class="text-black text-decoration-none fw-light">I will do ios and android mobile app development using flutter...</a></p>
+
+          <p><i class="fa-solid fa-star text-success"></i> 4.9 (17) <sub class="fs-5 float-end" style="margin-right: 42px !important;">From PKR 35,299</sub></p>
+
+        </div>
+
+        <div class="col p-1">
+
+          <a href="#"><img class="rounded" src="./img/recent/graphic_designs.jpg"></a>
+
+          <span><a href="#" class="text-black text-decoration-none">Waheed Ahmed <pre style="margin-right: 28px !important; margin-top: 2px;" class="badge text-bg-success d-inline float-end">Level 1</pre></a></span> <br>
+
+          <p><a href="#" class="text-black text-decoration-none fw-light">I will do ios and android mobile app development using flutter...</a></p>
+
+          <p><i class="fa-solid fa-star text-success"></i> 4.9 (17) <sub class="fs-5 float-end" style="margin-right: 42px !important;">From PKR 35,299</sub></p>
+
+        </div>
+
+      </div>
+    </div>
+
+   <!-- ------------- -->
+
+   <!-- service provide section -->
+   <div class="container">
+   <h1 class="my-5 fs-2 fw-light">A whole world of freelance <br> talent at your fingertips</h1>
+   <div class="row float-center">
+
+    <div class="col">
+      
+      <img src="./img/services/commitment-icon.svg" alt="handShake_image" width="20%" class="mb-3">
+      <h5>Clear, transparent pricing</h5>
+      <p>Better options within your budget are available. Pay per project or by the hour. Payments only get released when you approve.</p>
+
+  </div>
+
+  <div class="col">
+
+    <img src="./img/services/chatbot-icon.svg" alt="chatBot_image" width="20%" class="mb-2">
+    <h5>24/7 Chat support</h5>
+    <p>Chat with our team to get your questions answered or resolve any issues with your orders you can chat with chatbot.</p>
+
+  </div>
+
+    <div class="col">
+
+      <img src="./img/services/thunder-icon.svg" alt="spark(thunderBolt)_image" width="20%">
+      <h5>Quality work done faster</h5>
+      <p>Filter to find the right freelancers quickly and get great work delivered in no time, every time.</p>
+
+    </div>
+
+    <div class="col">
+
+      <img src="./img/services/protect-icon.svg" alt="trusted(shield)_image" width="20%">
+      <h5>Trusted & totally secured</h5>
+      <p>Once you join as a seller or buyer, your account details will be completely secure. You don't need to worry about your account.</p>
+
+    </div>
+
+   </div>
+
+   </div>
+    <!-- -------------------- -->
+
+    <!-- About Flexx Section -->
+
+
+
+    <!-- ------------------ -->
+
+    <!-- Made on Flexx Section -->
+
+    
+
+    <!-- --------------------- -->
+
+    <!-- Footer -->
+    <!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Company name
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+
+    <!-- ------- -->
+
+<!-- ------ -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
